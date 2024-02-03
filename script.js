17a194746af8cfb468efad6609663e95ba55311a
NikhilPeddireddy <139708292+NikhilPeddireddy@users.noreply.github.com>
2024-02-03 16:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var userConfirmation = confirm("Do you want to proceed?");
+
+// Che
+if (userConfirmation) {
+    // Display alert if OK is clicked
+    alert("You clicked OK. Proceeding...");
+} else {
+    // Display alert if Cancel is clicked
+    alert("You clicked Cancel. Exiting...");
+}
